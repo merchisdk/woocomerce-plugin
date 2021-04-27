a4c04541404c150706c9d642aa406137a22f1f04
Daniel Collins <mail@dcollins.info>
2021-04-28 03:54:31 +1000

    fix undefined redirectAfterSuccessUrl

--- assets/show_product.js
+++ assets/show_product.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(event) {
   const mountPointId = merchiShowProductScriptOptions.mountPointId;
   const productId = merchiShowProductScriptOptions.productId;
+  const redirectAfterSuccessUrl = merchiShowProductScriptOptions.redirectAfterSuccessUrl;
   const mountpoint = document.getElementById(mountPointId);
   const script = document.createElement('script');
   script.src = `https://merchi.co//static/product_embed/js/product.embed.js?product=${productId}&hidePreview=true&hideTitle=true&hideInfo=true&hidePrice=true&includeBootstrap=false&singleColumn=true&notIncludeDefaultCss=true&redirectAfterSuccessUrl=${redirectAfterSuccessUrl}"`;
--- merchi-plugin.php
+++ merchi-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Merchi Plugin
  * Plugin URI:        https://merchi.co
  * Description:       Fetch your products from Merchi. This plugin requires Woocommerce.
- * Version:           1.3.0
+ * Version:           1.3.1
  * Author:            Charlie Campton
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html

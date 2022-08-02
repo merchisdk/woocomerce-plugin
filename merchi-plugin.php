2296b1b05b3ba4be6491bcf31abee72ed3ab4955
alexdzyaba <alexdzyaba@gmail.com>
2022-08-02 12:45:50 +0300

    Change plugin version to 1.4.0

--- merchi-plugin.php
+++ merchi-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Merchi Plugin
  * Plugin URI:        https://merchi.co
  * Description:       Fetch your products from Merchi. This plugin requires Woocommerce.
- * Version:           1.3.2
+ * Version:           1.4.0
  * Author:            Charlie Campton
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
